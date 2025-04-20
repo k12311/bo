3a319965db7964177b3d7e9394f387feb9f29f17
k12311 <154042981+k12311@users.noreply.github.com>
2025-04-21 00:33:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,44 @@
-const CACHE_NAME = 'bro-cache-v1';
-const FILES_TO_CACHE = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png',
-  'bg1.jpg',
-  'bg2.jpg',
-  'bg3.jpg',
-  'bg4.jpg',
-  'bg5.jpg',
-  'bg6.jpg',
-  'bg7.jpg',
-  'https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=ZCOOL+KuaiLe&display=swap'
+const CACHE_NAME = "bro-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./bg1.jpg",
+  "./bg2.jpg",
+  "./bg3.jpg",
+  "./bg4.jpg",
+  "./bg5.jpg",
+  "./bg6.jpg",
+  "./bg7.jpg",
+  "https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=ZCOOL+KuaiLe&display=swap"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
         })
       )
     )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
   );
 });
