186a9397129e8cdd3037dbb60c9ba5a515d2753f
k12311 <154042981+k12311@users.noreply.github.com>
2025-04-21 00:11:50 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,46 @@
+const CACHE_NAME = 'bro-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'bg1.jpg',
+  'bg2.jpg',
+  'bg3.jpg',
+  'bg4.jpg',
+  'bg5.jpg',
+  'bg6.jpg',
+  'bg7.jpg',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=ZCOOL+KuaiLe&display=swap'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('bro-cache').then(cache => cache.addAll(['index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
